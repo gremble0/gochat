f032181883222e46a29d0d0816b736791ef97c77
gremble0 <hermanstornes@gmail.com>
2023-11-03 19:02:28 +0100

    File structure changes

--- src/main.go
+++ src/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"log"
 	"net"
-	"./client"
+	"fmt"
 )
 
 // IDEAS:
@@ -24,6 +24,7 @@ const (
 	Send
 )
 
+
 type Message struct {
 	Type   MessageType
 	Sender Client
@@ -33,6 +34,88 @@ type Message struct {
 type Client struct {
 	Username string
 	Conn     net.Conn
+}
+
+func runClient(conn net.Conn, messages chan Message) {
+	buf := make([]byte, 256)
+	conn.Write([]byte("Welcome to go-chat! Please enter a username: "))
+
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Printf("Could not read username from: %s\n", conn.RemoteAddr())
+		conn.Close()
+		return
+	}
+
+	client := Client{
+		Username: string(buf[0 : n-1]),
+		Conn:     conn,
+	}
+
+	messages <- Message{
+		Type:   Connect,
+		Sender: client,
+	}
+
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			messages <- Message{
+				Type:   Disconnect,
+				Sender: client,
+			}
+			return
+		}
+
+		messages <- Message{
+			Type:   Send,
+			Sender: client,
+			Text:   string(buf[0 : n-1]),
+		}
+	}
+}
+func server(messages chan Message) {
+	clients := map[string]*Client{}
+	for {
+		message := <-messages
+		switch message.Type {
+
+		case Connect:
+			clients[message.Sender.Conn.RemoteAddr().String()] = &message.Sender
+
+			outstr := "New user joined with username '" + message.Sender.Username + "'\n"
+			log.Printf(outstr)
+
+			for _, client := range clients {
+				if client.Conn.RemoteAddr().String() != message.Sender.Conn.RemoteAddr().String() {
+					client.Conn.Write([]byte(outstr))
+				}
+			}
+
+		case Disconnect:
+			message.Sender.Conn.Close()
+			delete(clients, message.Sender.Conn.RemoteAddr().String())
+
+			outstr := fmt.Sprintf("User '%s@%s' has disconnected\n", message.Sender.Username, message.Sender.Conn.RemoteAddr())
+			log.Printf(outstr)
+			for _, client := range clients {
+				if client.Conn.RemoteAddr().String() != message.Sender.Conn.RemoteAddr().String() {
+					client.Conn.Write([]byte(outstr))
+				}
+			}
+
+		case Send:
+			outStr := message.Sender.Username + ": " + message.Text + "\n"
+			log.Printf(outStr)
+
+			for _, client := range clients {
+				if client.Conn.RemoteAddr().String() != message.Sender.Conn.RemoteAddr().String() {
+					client.Conn.Write([]byte(outStr))
+				}
+			}
+
+		}
+	}
 }
 
 func main() {
@@ -52,6 +135,6 @@ func main() {
 			continue
 		}
 
-		go client(conn, messages)
+		go runClient(conn, messages)
 	}
 }
