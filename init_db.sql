1e63a90ff798ce5a4be5665b91bbe40fc76e4a93
gremble0 <hermanstornes@gmail.com>
2023-11-15 08:23:13 +0100

    Added composite primary key for messages table

--- init_db.sql
+++ init_db.sql
@@ -9,16 +9,17 @@ COMMENT ON DATABASE gochat
 
 -- Initialize tables
 CREATE TABLE users (
-    user_id BIGSERIAL NOT NULL PRIMARY KEY,
     username VARCHAR (20) NOT NULL,
     remote_addr VARCHAR (64) NOT NULL,
-    registered TIMESTAMP
+    registered TIMESTAMP,
+    PRIMARY KEY (username, remote_addr)
 );
 
 CREATE TABLE messages (
     message_id BIGSERIAL NOT NULL PRIMARY KEY,
     message VARCHAR (255) NOT NULL,
-    user_id INT NOT NULL,
+    sender VARCHAR (20) NOT NULL,
+    sender_addr VARCHAR (64) NOT NULL,
     sent TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users (user_id)
+    FOREIGN KEY (sender, sender_addr) REFERENCES users (username, remote_addr)
 );
